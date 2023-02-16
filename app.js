71302385822f00b89d4efc537e0fb5bb2b3466eb
Mckenkoe <koetitz_mckenna@columbusstate.edu>
2023-02-16 00:37:30 +0000

    added api and axios

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var app = express();
+var axios = require('axios');
 
+var key = "788aa04875544ccabbf41651231402";
+
+//get forecast for Orlando
 app.get('/', function(req, res){
-    res.send('Hello World');
+    axios.get("http://api.weatherapi.com/v1/forecast.json?key="+key+"&q=Orlando&days=2").then(function(res){
+        //outputs Orlandos current temp in F
+        console.log("Orlando current temp: "+res.data.current.temp_f);
+        //Outputs the today and tomorrows forecast max tempeture in F
+        console.log(res.data.forecast.forecastday[0].day.maxtemp_f);
+        console.log(res.data.forecast.forecastday[1].day.maxtemp_f);
+        
+    })
 })
+
+// app.get('/', function(req, res){
+//     res.send('Hello World');
+// })
+
+
 
 app.listen(3000, function(){
     console.log('App listening on port 3000')
