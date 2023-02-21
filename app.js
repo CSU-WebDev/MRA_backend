dac4d836c702a43f81f565d8b658f500130a05d7
Mckenkoe <koetitz_mckenna@columbusstate.edu>
2023-02-21 23:58:09 +0000

    send full json

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ var axios = require('axios');
 var key = "788aa04875544ccabbf41651231402";
 
 //get forecast for Orlando
-app.get('/', function(req, res){
-    axios.get("http://api.weatherapi.com/v1/forecast.json?key="+key+"&q=Orlando&days=2").then(function(res){
+ app.get('/', function(req, res){
+    axios.get("http://api.weatherapi.com/v1/forecast.json?key="+key+"&q=Orlando&days=1").then(function(resp){
         // //outputs Orlandos current temp in F
         // console.log("Orlando current temp: "+res.data.current.temp_f);
         // //Outputs the today and tomorrows forecast max tempeture in F
         // console.log(res.data.forecast.forecastday[0].day.maxtemp_f);
         // console.log(res.data.forecast.forecastday[1].day.maxtemp_f);
-        console.log(res.data)
-        return res.data;
-        
+        console.log(resp.data)
+        //return res.data;
+        res.json(resp.data);
     })
+    
 })
 
 // app.get('/', function(req, res){
