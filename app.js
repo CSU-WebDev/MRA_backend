f4d007d29dc700d9aa6fce065f890c8dc0704fa6
rajk75 <rajkotak75@icloud.com>
2023-03-14 22:50:57 +0000

    added app.use functions & moved costant options to get them before the key is retrieved

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ var app = express();
 var axios = require('axios');
 const cors=require('cors');
 
+const options = {
+
+    "origin": "*",
+    
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    
+    "preflightContinue": true,
+    
+    "optionsSuccessStatus": 200
+    
+    }
+app.use(express.json())
+app.use(cors(options))
+
 var key = "788aa04875544ccabbf41651231402";
 
 //send json
@@ -30,17 +44,7 @@ var key = "788aa04875544ccabbf41651231402";
 // })
 
 //cors
-const options = {
 
-    "origin": "*",
-    
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    
-    "preflightContinue": true,
-    
-    "optionsSuccessStatus": 200
-    
-    }
 
 app.use(cors(options))
 
