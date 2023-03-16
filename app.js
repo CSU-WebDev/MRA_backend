6f86086e08f87a3f713fd0e6c0dc8f9e9c1883a1
Mckenkoe <koetitz_mckenna@columbusstate.edu>
2023-03-16 02:29:09 +0000

    added error handling

--- app.js
+++ app.js
@@ -26,17 +26,7 @@ var key = "788aa04875544ccabbf41651231402";
         //console.log(resp.data);
         let weather_data = resp.data;
         let hour_objects = []
-        // weather_data.forecast.forecastday.hour.forEach(hour => {
-        //     var x = {
-        //         time : hour.time,
-        //         tempF : hour.temp_f,
-        //         wind : hour.wind_mph,
-        //         precip : hour.precip_in,
-        //         humidity : hour.humidity
-        //     }
 
-        //     hour_objects.push(x);
-        // });
         let arrayHours = weather_data.forecast.forecastday[0].hour
         for(let i = 0; i < arrayHours.length;i++){
             var x = {
@@ -58,28 +48,30 @@ var key = "788aa04875544ccabbf41651231402";
         res.json(forecast);
     })
     //catch error
-    
+    .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
 })
 
-//build own object of data needed
-
-// app.get('/', function(req, res){
-//     axios.get("http://api.weatherapi.com/v1/forecast.json?key="+key+).then(function(resp){
-        
-//     })
-    
-// })
-
-// app.get('/', function(req, res){
-//     res.send('Hello World');
-// })
-
 //cors
-
-
 app.use(cors(options))
 
 
-app.listen(3000, function(){
-    console.log('App listening on port 3000')
+app.listen(4000, function(){
+    console.log('App listening on port 4000')
 })
